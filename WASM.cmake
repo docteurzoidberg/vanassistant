f3757c670f8999db6508f2e1be082ad884c6c27e
DrZoid <docteurzoidberg@users.noreply.github.com>
2024-07-13 02:59:25 +0200

    working checkpoint after buildroot integration

--- WASM.cmake
+++
@@ -1,8 +0,0 @@
-
-# Specify the compiler for Emscripten
-set(CMAKE_C_COMPILER emcc)
-set(CMAKE_CXX_COMPILER em++)
-
-# Additional compiler flags
-set(PLATFORM_CXX_FLAGS "-O2")
-set(PLATFORM_LIBS "embind")
--- X11.cmake
+++
@@ -1,8 +0,0 @@
-
-# Specify the compiler for X11
-set(CMAKE_C_COMPILER gcc)
-set(CMAKE_CXX_COMPILER g++)
-
-# Additional compiler flags
-#set(PLATFORM_CXX_FLAGS "-Wall -O2")
-#set(PLATFORM_LIBS "X11 pge")
