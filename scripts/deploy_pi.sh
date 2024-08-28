9cccdf97ba0234198dbe6b23ffcd74ba7923e33a
DrZoid <drzoid@tppt.eu>
2024-08-28 17:43:06 +0200

    CMakeLists for PI_FB and PI_X11

--- scripts/deploy_pi.sh
+++ scripts/deploy_pi.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #kill the existing process if any
-ssh pizerodev "killall vanassistant.pios"
+ssh pizerodev "killall vanassistant.pi_fb"
 
 #copy the binary to the pi /opt folder
-scp ./vanassistant.pios pizerodev:/opt/vanassistant
+scp ./vanassistant.pi_fb pizerodev:/opt/vanassistant
 
 #kill any existing process and launch the new one
-ssh -f pizerodev "sh -c 'cd /opt/vanassistant; nohup ./vanassistant.pios /dev/ttyAMA0 115200 > /dev/null 2>&1 &'"
+ssh -f pizerodev "sh -c 'cd /opt/vanassistant; nohup ./vanassistant.pi_fb /dev/ttyAMA0 115200 > /dev/null 2>&1 &'"
