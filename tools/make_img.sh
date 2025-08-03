231d861f95c5e52d5650a1403389545107c02d6d
DrZoid <drzoid@tppt.eu>
2025-08-03 11:29:53 +0200

    removed scripts (included in cmake)

--- tools/make_img.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cwd=$(pwd)
-cd /home/drzoid/rpi-zero-minimal-buildroot/build_workdir
-make -j4
-cp /home/drzoid/rpi-zero-minimal-buildroot/build_workdir/images/sdcard.img $cwd/vanassistant.sdcard.img
